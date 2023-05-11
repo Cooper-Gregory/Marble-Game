d3d8aeaeb1bc5b40bf5d083136b46bd2a8e48593
Cooper-Gregory <cooper.c.gregory@gmail.com>
2023-05-11 16:32:13 +1000

    Fixed doors

--- Assets/Scripts/GameObjective.cs
+++ Assets/Scripts/GameObjective.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameObjective : MonoBehaviour
 {
     public int objectiveID;
     public string objectiveName;
-    private string ogMessage;
     public bool objectiveActive;
     public bool objectiveCompleted;
     public int objectivePartsTotal;
     public int objectiveStep;
+
+    private Text objMessageField;
+    private Text objDoneMessageField;
+
+
 
     [Header("Events")]
     public GameEvent onObjectiveActivated;
@@ -21,44 +26,87 @@ public class GameObjective : MonoBehaviour
     private void Awake()
     {
         objManager = GameObject.Find("ObjectiveManager");
+        //objMessageField = objManager.GetComponent<ObjectiveTracker>().objMessageField;
+        //objDoneMessageField = objManager.GetComponent<ObjectiveTracker>().objDoneMessageField;
+
+
         if (objectiveActive)
         {
             StartQuest();
         }
-        ogMessage = objectiveName;
+
+
     }
 
+    // THIS FUNCTION STARTS AN OBJECTIVE, ADDS IT TO THE OBJECTIVE UI, CALLS UI/SOUND FEEDBACK, MARKS IT AS ACTIVE
     public void StartQuest()
     {
         Debug.Log("Objective " + objectiveName + " started.");
         objectiveActive = true;
-        SetObjectiveString();
+        //StartCoroutine(ShowStartMessage());
         objManager.GetComponent<ObjectiveTracker>().AddObjective(this);
-        onObjectiveActivated.Raise(this, null);
+        if (onObjectiveActivated != null)
+        {
+            onObjectiveActivated.Raise(this, null);
+        }
+
+
+
     }
 
-    // Update is called once per frame
+    // THIS FUNCTION MOVES AN ACTIVE OBJECTIVE ONE 'STEP' AHEAD - THIS FINISHES ANY QUEST NOT ABOUT DOING EXACTLY THE SAME THING X TIMES.
     public void AddQuestStep()
     {
-        objectiveStep++;
-        SetObjectiveString(); // might not update UI (why would it?)
-        objManager.GetComponent<ObjectiveTracker>().RemoveObj(objectiveID);
-        objManager.GetComponent<ObjectiveTracker>().AddObjective(this);
-
-        if (objectiveStep == objectivePartsTotal)
+        if (objectiveActive)
         {
-            objectiveCompleted = true;
-            objManager.GetComponent<ObjectiveTracker>().RemoveObj(objectiveID);
-            Debug.Log("Objective " + objectiveName + " complete.");
+            objectiveStep++;
+            if (objectiveStep == objectivePartsTotal)
+            {
+                objectiveCompleted = true;
+                //StartCoroutine(ShowEndMessage());
+
+                if (onObjectiveCompleted != null)
+                {
+                    onObjectiveCompleted.Raise(this, null);
+                }
+                objManager.GetComponent<ObjectiveTracker>().RemoveObj(objectiveID);
+                Debug.Log("Objective " + objectiveName + " complete.");
+            }
         }
+
     }
 
-    public void SetObjectiveString()
+    IEnumerator ShowStartMessage()
     {
-        if (objectivePartsTotal > 1)
+        if (objMessageField.text != null)
         {
-            objectiveName = ogMessage + objectiveStep + " of " + objectivePartsTotal;
+            objMessageField.text = "New objective: " + objectiveName;
+            yield return new WaitForSeconds(5); // ADJUST THIS NUMBER TO MAKE OBJECTIVE STARTING TEXT STAY LONGER/SHORTER
+            objMessageField.text = null;
+        }
+        else
+        {
+            objMessageField.text = "Multiple new objectives!";
+            yield return new WaitForSeconds(5);
+            objMessageField.text = null;
+        }
+    }
+
+    IEnumerator ShowEndMessage()
+    {
+        if (objDoneMessageField.text != null)
+        {
+            objDoneMessageField.text = "Objective complete: " + objectiveName;
+            yield return new WaitForSeconds(5); // ADJUST THIS NUMBER TO MAKE OBJECTIVE ENDING TEXT STAY LONGER/SHORTER
+            objDoneMessageField.text = null;
+        }
+        else
+        {
+            objDoneMessageField.text = "Multiple objectives completed!";
+            yield return new WaitForSeconds(5);
+            objDoneMessageField.text = null;
         }
 
     }
+
 }
