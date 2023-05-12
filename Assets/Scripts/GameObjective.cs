7e1859f124001b4e3360a5ebab33d049a350c8b4
Cooper-Gregory <cooper.c.gregory@gmail.com>
2023-05-12 11:55:47 +1000

    Objective collective counter restored

--- Assets/Scripts/GameObjective.cs
+++ Assets/Scripts/GameObjective.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class GameObjective : MonoBehaviour
 {
     public int objectiveID;
     public string objectiveName;
+    private string ogMessage;
     public bool objectiveActive;
     public bool objectiveCompleted;
     public int objectivePartsTotal;
@@ -35,7 +37,7 @@ public class GameObjective : MonoBehaviour
             StartQuest();
         }
 
-
+        ogMessage = objectiveName;
     }
 
     // THIS FUNCTION STARTS AN OBJECTIVE, ADDS IT TO THE OBJECTIVE UI, CALLS UI/SOUND FEEDBACK, MARKS IT AS ACTIVE
@@ -43,6 +45,7 @@ public class GameObjective : MonoBehaviour
     {
         Debug.Log("Objective " + objectiveName + " started.");
         objectiveActive = true;
+        SetObjectiveString();
         //StartCoroutine(ShowStartMessage());
         objManager.GetComponent<ObjectiveTracker>().AddObjective(this);
         if (onObjectiveActivated != null)
@@ -60,6 +63,9 @@ public class GameObjective : MonoBehaviour
         if (objectiveActive)
         {
             objectiveStep++;
+            SetObjectiveString(); 
+            objManager.GetComponent<ObjectiveTracker>().RemoveObj(objectiveID); // REMOVING OBJECTIVE TO CLEAR IT FROM HUD
+            objManager.GetComponent<ObjectiveTracker>().AddObjective(this); // ADDING IT AGAIN INSTANTLY, BUT NOW WITH UPDATED TEXT STRING FROM 2 LINES ABOVE
             if (objectiveStep == objectivePartsTotal)
             {
                 objectiveCompleted = true;
@@ -72,6 +78,16 @@ public class GameObjective : MonoBehaviour
                 objManager.GetComponent<ObjectiveTracker>().RemoveObj(objectiveID);
                 Debug.Log("Objective " + objectiveName + " complete.");
             }
+        }
+
+    }
+
+    public void SetObjectiveString()
+    {
+        if (objectivePartsTotal > 1)
+        {
+            objectiveName = ogMessage + objectiveStep + " of " + objectivePartsTotal;
+            Debug.Log("Update TYME!");
         }
 
     }
